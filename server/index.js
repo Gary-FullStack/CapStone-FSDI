6164aab0d4ceb241c9bbc917870fef285a241563
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-05-05 09:22:29 -0400

    connecting to DB
    get that that Mongo up!

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+
+// database connection
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() =>console.log("DB connected"))
+.catch((err) => console.log("DB Error"));
 
 app.get("/", (req, res) => {
     res.json({
